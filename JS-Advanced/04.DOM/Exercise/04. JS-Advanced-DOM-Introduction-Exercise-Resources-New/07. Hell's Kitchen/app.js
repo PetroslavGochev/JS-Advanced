0c665d7daee01daf23572a997f76114e0ec8ba47
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-10-05 15:11:01 +0300

    Js-Advanced

--- JS-Advanced/04.DOM/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ JS-Advanced/04.DOM/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,9 +1,64 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
+   onClick();
+   function onClick() {
+      //    let test = '["PizzaHut - Peter 500, George 300, Mark 800","TheLake - Bob 1300, Joe 780, Jane 660"]';
       let textArea = document.getElementById('inputs').getElementsByTagName('textarea')[0].value;
-
-      let collection  = textArea.split('\n');
+      if(textArea != ''){
+         let result = {};
+         let collection = textArea.replace('"]', '').split('","');
+   
+         for (const restaurantData of collection) {
+   
+            let restaurant = restaurantData.split(' - ');
+            let nameOfRestaurant = restaurant.shift().replace('["', '');
+   
+            if (!result[nameOfRestaurant]) {
+               result[nameOfRestaurant] = [];
+            }
+            let workersData = [];
+            if (restaurant.length > 0) {
+               workersData = restaurant[0].split(', ');
+            }
+   
+            for (let index = 0; index < workersData.length; index++) {
+               let personData = workersData[index].split(' ');
+               let name = personData[0];
+               let salary = Number(personData[1]);
+   
+               let worker = {
+                  name: name,
+                  salary: salary,
+               }
+                  ;
+               result[nameOfRestaurant].push(worker);
+            }
+            result[nameOfRestaurant].sort(function (a, b) { return b.salary - a.salary });
+         }
+         let restaurantAverageSalary = [];
+         for (const [key, value] of Object.entries(result)) {
+            let averageSalary = 0;
+            for (const salary of value) {
+               averageSalary += salary.salary;
+            }
+            averageSalary /= value.length;
+            restaurantAverageSalary.push([key, averageSalary]);
+         }
+         restaurantAverageSalary.sort(function (a, b) { return b[1] - a[1] });
+   
+         let bestRestaurantName = restaurantAverageSalary[0][0];
+         let bestRestaurantAverage = restaurantAverageSalary[0][1].toFixed(2);
+         let bestSalary = Math.round(result[bestRestaurantName][0].salary);
+         let bestRestaurantParagraph = document.getElementById('bestRestaurant').getElementsByTagName('p')[0];
+         bestRestaurantParagraph.textContent = `Name: ${bestRestaurantName} Average Salary: ${bestRestaurantAverage} Best Salary: ${bestSalary.toFixed(2)}`;
+   
+         // console.log(bestRestaurantParagraph);
+   
+         let workers = document.getElementById('workers').getElementsByTagName('p')[0];
+         workers.textContent = result[bestRestaurantName].map(w => `Name: ${w.name} With Salary: ${w.salary}`).join(' ');
+   
+         // console.log(workers);
+      }
    }
 }
--- JS-Advanced/04.DOM/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ JS-Advanced/04.DOM/Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,30 @@
 function generateReport() {
-    //TODO
+    let collection = document.querySelectorAll("input[type='checkbox']");
+    let checkedHeaders = {};
+
+    let result = [];
+
+    Array.from(collection);
+    for (let index = 0; index < collection.length; index++) {
+        if (collection[index].checked) {
+            checkedHeaders[index] = collection[index].name;
+        }
+    }
+
+    let column = document.querySelectorAll('tr');
+    Array.from(column);
+
+    for (let index = 1; index < column.length; index++) {
+        let columnData = column[index].getElementsByTagName('td');
+        let obj = {};
+        for (let x = 0; x < columnData.length; x++) {
+            if(checkedHeaders[x]){
+                let propertyName = checkedHeaders[x];
+                let value = columnData[x].textContent;         
+                obj[propertyName] = value;             
+            }        
+        }
+        result.push(obj);
+    }
+    document.getElementById('output').textContent = JSON.stringify(result);
 }
