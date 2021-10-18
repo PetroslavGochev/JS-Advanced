ec89d36dd3fb6e84d2eb5790c8a21ed7e5c83f35
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-10-18 23:19:48 +0300

    Js-Advanced

--- JS-Advanced/05.DOM Manipulation and Events/Exercise/01. Sections/script.js
+++ JS-Advanced/05.DOM Manipulation and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+   let div;
+   let paragraph;
+
+   for (let index = 0; index < words.length; index++) {
+      div = document.createElement('div');
+       
+      paragraph = document.createElement('p');
+      paragraph.innerHTML = words[index];
+      paragraph.style.display = 'none';
+      div.appendChild(paragraph);
+      document.getElementById('content').appendChild(div);
+
+      div.addEventListener('click',onClick);
+   }
+
+   function onClick(event){
+      event.target.getElementsByTagName('p')[0].style.display = '';
+   }
 }
