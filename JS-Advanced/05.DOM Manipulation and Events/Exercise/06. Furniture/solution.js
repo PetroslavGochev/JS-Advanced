407893fbd83e962427f4681e27413366fef77d8c
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-10-24 21:47:05 +0300

    Js-Advanced

--- JS-Advanced/05.DOM Manipulation and Events/Exercise/06. Furniture/solution.js
+++ JS-Advanced/05.DOM Manipulation and Events/Exercise/06. Furniture/solution.js
@@ -1,4 +1,44 @@
 function solve() {
+  let buttons = document.querySelectorAll('button');
 
-  //TODO...
+  buttons[0].addEventListener('click', furniture)
+  buttons[1].addEventListener('click', buy)
+
+  let table = document.querySelector('.table tbody');
+
+  function furniture(event){
+    let array = JSON.parse(document.querySelector('textarea').value); 
+    for (const item of array) {
+      //create new TR in tbody with data from the object 
+      let tr = document.createElement('tr');
+      table.appendChild(tr);
+
+      //can be done with FOREACH on object properties... or with another function
+      let tdData=document.createElement('td');
+      tdData.innerHTML='<img src="'+item.img+'"/>';
+      tr.appendChild(tdData);
+
+      tdData=document.createElement('td');
+      tdData.textContent=item.name;            
+      tr.appendChild(tdData);
+
+      tdData=document.createElement('td');
+      tdData.textContent=item.price;            
+      tr.appendChild(tdData);
+
+      tdData=document.createElement('td');
+      tdData.textContent=item.decFactor;            
+      tr.appendChild(tdData);
+
+      tdData=document.createElement('td');
+      tdData.innerHTML='<input type="checkbox" />';
+      tr.appendChild(tdData);
+
+
+  }
+  }
+
+  function buy(event){
+
+  }
 }
